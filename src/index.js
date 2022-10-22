ead7eecc844fdfd9afdde5501a95ea60306e6944
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2022-10-22 20:00:06 +0300

    hidden

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 console.log("Launching script");
 const anchors = document.querySelectorAll(".thumbnails-anchor");
-// const anchors1 = document.querySelectorAll(".details-container");
+const anchors1 = document.querySelectorAll(".details-container");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const mainClass = document.querySelector(".main-class");
@@ -16,12 +16,12 @@ for (let i = 0; i < anchors.length; i++) {
         setDetails(anchors[i]);
     });
 }
-// for (let i = 0; i < anchors1.length; i++) {
-//     anchors1[i].addEventListener("click", function () {
-//         console.log("event - click on", anchors1[i]);
-//         setDetails(anchors1[i]);
-//     });
-// }
+for (let i = 0; i < anchors1.length; i++) {
+    anchors1[i].addEventListener("click", function () {
+        console.log("event - click on", anchors1[i]);
+        setDetails(anchors1[i]);
+    });
+}
 
 function showDetails() {
     mainClass.classList.remove("hidden");
