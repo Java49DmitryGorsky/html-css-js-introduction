dbddc1be5e0df7f2e876aa7de9bde33a8859be83
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2022-10-22 19:44:22 +0300

    hidden

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 console.log("Launching script");
 const anchors = document.querySelectorAll(".thumbnails-anchor");
-const anchors1 = document.querySelectorAll(".details-container");
+// const anchors1 = document.querySelectorAll(".details-container");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const mainClass = document.querySelector(".main-class");
+const HIDDEN = "hidden";
 function setDetails(anchor) {
     detailsImage.setAttribute("src", anchor.getAttribute("data-details-image"));
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
@@ -10,12 +12,20 @@ function setDetails(anchor) {
 for (let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function () {
         console.log("event - click on", anchors[i]);
+        showDetails();
         setDetails(anchors[i]);
     });
 }
-for (let i = 0; i < anchors1.length; i++) {
-    anchors1[i].addEventListener("click", function () {
-        console.log("event - click on", anchors1[i]);
-        setDetails(anchors1[i]);
-    });
+// for (let i = 0; i < anchors1.length; i++) {
+//     anchors1[i].addEventListener("click", function () {
+//         console.log("event - click on", anchors1[i]);
+//         setDetails(anchors1[i]);
+//     });
+// }
+
+function showDetails() {
+    mainClass.classList.remove("hidden");
+}
+function hideDetails() {
+    mainClass.classList.add(HIDDEN);
 }
