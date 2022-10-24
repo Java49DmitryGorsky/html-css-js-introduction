030bc3cbecc8d9f4151a4ea288618a983aee289a
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2022-10-24 18:03:05 +0300

    test 10-AnimationTransformTransition

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const mainClass = document.querySelector(".main-class");
+const detailsContainer = document.querySelector(".details-container");
+// var x = document.getElementById("myAudio"); 
 const HIDDEN = "hidden";
+const IS_POINT = "is-point";
+
 function setDetails(anchor) {
- detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
- detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
+    detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
+    detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
 }
 
-for(let i = 0; i < anchors.length; i++) {
-    anchors[i].addEventListener("click", function(){
+for (let i = 0; i < anchors.length; i++) {
+    anchors[i].addEventListener("click", function () {
         console.log("event - click on", anchors[i]);
         showDetails();
         setDetails(anchors[i]);
@@ -19,7 +23,20 @@ for(let i = 0; i < anchors.length; i++) {
 
 function showDetails() {
     mainClass.classList.remove("hidden")
+    detailsContainer.classList.add(IS_POINT);
+    setTimeout(function () {
+        detailsContainer.classList.remove(IS_POINT);
+    }, 1)
 }
 function hideDetails() {
     mainClass.classList.add(HIDDEN);
 }
+
+// function playAudio() {
+//     x.play();
+// }
+
+function OnLoad() {
+    //On Load Of Image
+         document.getElementById('audio').play();       
+    }
