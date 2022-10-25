3cf92eeb1e6be4c83e9e11f6bb34c459d32d08bb
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2022-10-25 19:46:01 +0300

    Edited on webinar 10

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ function setDetails(anchor) {
 for (let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function () {
         console.log("event - click on", anchors[i]);
-        showDetails();
         setDetails(anchors[i]);
+        showDetails();   
     })
 }
 
