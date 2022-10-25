07ffc5051797c66141a40d28140cc50f55756323
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2022-10-25 13:42:42 +0300

    HW AnimationTransformTransition

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ const detimg = document.querySelector(".details-container");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const mainClass = document.querySelector(".main-class");
+const detailsContainer = document.querySelector(".details-container");
+// const sounds = document.querySelectorAll("audio") //sound
+// var element = document.getElementById('audio');
 const HIDDEN = "hidden";
+const IS_POINT = "is-point";
+
 function setDetails(anchor) {
     detailsImage.setAttribute("src", anchor.getAttribute("data-details-image"));
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
 }
+
 for (let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function () {
         console.log("event - click on", anchors[i]);
@@ -19,7 +25,12 @@ for (let i = 0; i < anchors.length; i++) {
 
 function showDetails() {
     mainClass.classList.remove("hidden");
+    detailsContainer.classList.add(IS_POINT);
+    setTimeout(function () {
+        detailsContainer.classList.remove(IS_POINT);
+    }, 1)
 }
+
 function hideDetails() {
     mainClass.classList.add(HIDDEN);
 }
@@ -28,3 +39,53 @@ detimg.addEventListener("click", function () {
     console.log("event - click on", detimg);
     setDetails(detimg);
 });
+
+// function OnLoad() {
+//     //On Load Of Image
+//     document.getElementById('myAudio').play();
+// }
+
+function playSound() {
+    document.getElementById(ship_sound).play();
+  };
+function playSound(in_mountains) {
+    document.getElementById(in_mountains).play();
+  };
+  function playSound(calm_surf) {
+    document.getElementById(calm_surf).play();
+  };
+  function playSound(surf_noise) {
+    document.getElementById(surf_noise).play();
+  };
+  function playSound(sea_sunset) {
+    document.getElementById(sea_sunset).play();
+  };
+  function playSound(find_me) {
+    document.getElementById(find_me).play();
+  };
+
+
+
+
+
+
+
+// function StartOrStop(audioFile) {
+//     var audio = document.getElementById("myAudio");
+//     if (!audio.src || audio.src !== audioFile) audio.src = audioFile;
+//     if (audio.paused == false)
+//         audio.pause();
+//     else
+//         audio.play();
+//     }
+
+
+
+
+// function playMySong(){
+//     if(audio.paused){
+//         audio.play()
+//     } else {
+//         audio.pause()
+//     }
+// }
