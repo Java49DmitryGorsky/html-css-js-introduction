d6f5b636cdfb72ac2c38c382eab8710c2ad80d04
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2022-10-28 17:03:43 +0300

    3

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-//  let num = 0123;
-// //  function sum_of_digit(n)
-// //  {
-// //      if (n == 0)
-// //      return 0;
-// //      return (n % 10 + sum_of_digit(parseInt(n / 10)));
-// //  }
+ let num = 623;
+ function sum_of_digit(n)
+ {
+     if (n == 0)
+     return 0;
+     return (n % 10 + sum_of_digit(parseInt(n / 10)));
+ }
 
-//  let result = sum_of_digit(num);
-//  console.log( "Sum of digits in "+ num +" is "+result );
+ let result = sum_of_digit(num);
+ console.log( "Sum of digits in "+ num +" is "+result );
 
 
 
-const sumDigits = (n = 0) =>
-{ if (n < 0)
-    if (n > -10)
-      return n
-    else
-      return (-n % 10) + sumDigits (n / 10 >> 0)
-  else
-    if (n < 10)
-      return n
-    else
-      return (n % 10) + sumDigits (n / 10 >> 0)
-}  
+// const sumDigits = (n = 0) =>
+// { if (n < 0)
+//     if (n > -10)
+//       return n
+//     else
+//       return (-n % 10) + sumDigits (n / 10 >> 0)
+//   else
+//     if (n < 10)
+//       return n
+//     else
+//       return (n % 10) + sumDigits (n / 10 >> 0)
+// }  
 
-console.log(sumDigits('-0123'))
+// console.log(sumDigits(-623))
