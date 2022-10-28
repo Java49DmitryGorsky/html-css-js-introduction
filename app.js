875aace517a4e910f5c718b4d632850ff5963e23
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2022-10-28 12:20:03 +0300

    HM 11

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-// console.log("Hello world");
-// for(var i = 0; i < 3; i++) {
-//     setTimeout(function(){
-//         console.log(i)
-//     }, (i+1) * 5000);
-// }
-// let i = 100;
-
-// for(let i = 0; i < 3; i++) {
-//     setTimeout(function() {console.log(i)});
-// }
-
-// let i;
-// for( var i = 0; i < 3; i++) {
-//     console.log(i);
-// }
-// console.log(i);
-
-
-  
-
  let num = 12345;
  function sum_of_digit(n)
  {
