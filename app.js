7b245003134a29d63684079f2154c8d5f1a76a61
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2022-10-29 18:45:50 +0300

    sum_of_digits non recursion

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
- let num = 623;
- function sum_of_digit(n)
- {
-     if (n == 0)
-     return 0;
-     if (n < 0)
-     n = -n;
-     return (n % 10 + sum_of_digit(parseInt(n / 10)));
- }
 
- let result = sum_of_digit(num);
- console.log( "Sum of digits in "+ num +" is "+result );
+//  let num = -0623;
+//  function sum_of_digit(n)
+//  {
+//      if (n == 0)
+//      return 0;
+//      if (n < 0)
+//      n = -n;
+//      return (n % 10 + sum_of_digit(parseInt(n / 10)));
+//  }
 
+//  let result = sum_of_digit(num);
+//  console.log( "Sum of digits in "+ num +" is "+result );
 
+function sumDigits(n) {
+    return `${n}`.match(/\d/g).reduce( (sum, digit) => sum + +digit, 0 );
+  }
+  
+  console.log( sumDigits( -623 ) );
 
-// const sumDigits = (n = 0) =>
-// { if (n < 0)
-//     if (n > -10)
-//       return n
-//     else
-//       return (-n % 10) + sumDigits (n / 10 >> 0)
-//   else
-//     if (n < 10)
-//       return n
-//     else
-//       return (n % 10) + sumDigits (n / 10 >> 0)
-// }  
-
-// console.log(sumDigits(-623))
+// function sum_of_digit(n) {      
+//     let sum = 0;
+//     for( let digit of `${n}` ){ // прямо тут, сразу превращая n в строку
+//       sum += +digit;
+//     }      
+//     return sum;
+//   }
+  
+//   console.log( sum_of_digit(-623) );
