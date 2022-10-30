9d8e0780766aa33df9919b0f9163f557aafe7e2a
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2022-10-30 19:55:39 +0300

    from lesson

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 
+function sumDigits(number) {
+    // if (number < 0) {
+    //     number = -number;
+    // }
+    number = Math.abs(number);
+    let sum = 0;
+    number = Math.trunc(number); //get read of the fractional part
+    do {
+        let digit = number % 10;
+        number = (number - digit) / 10;
+        // sum = sum + digit;
+        sum += digit;
+    } while(number !=0);
+    return sum;
+}
+let sum = sumDigits(-623.12345);
+console.log(sum);
+
+
 //  let num = -0623;
 //  function sum_of_digit(n)
 //  {
@@ -12,11 +31,11 @@
 //  let result = sum_of_digit(num);
 //  console.log( "Sum of digits in "+ num +" is "+result );
 
-function sumDigits(n) {
-    return `${n}`.match(/\d/g).reduce( (sum, digit) => sum + +digit, 0 );
-  }
+// function sumDigits(n) {
+//     return `${n}`.match(/\d/g).reduce( (sum, digit) => sum + +digit, 0 );
+//   }
   
-  console.log( sumDigits( -623 ) );
+//   console.log( sumDigits( -623 ) );
 
 // function sum_of_digit(n) {      
 //     let sum = 0;
