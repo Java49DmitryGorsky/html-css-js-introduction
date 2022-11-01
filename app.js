4c9f65c4239043d042051b51dccc94afa5db2c8d
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2022-11-01 13:55:41 +0300

    with_hm12

--- app.js
+++ app.js
@@ -71,29 +71,30 @@ i = 100;
 
 // conversion from Morse code to number
 // symbol "." -> 0 ; "-" -> 1
-function  fromMorseToNumber(morseCode) {
-    let result = 0;
-    for (let i = 0; i < morseCode.length; i++) {
-        let code = morseCode[i] == '.' ? 0 : 1;
-        result = result * 2 + code;
-    }
-    return result;
-}
-console.log(fromMorseToNumber('-.-...-.---.-.-..--'));
 
-function fromNumberToMorse(number) {
-    number = Math.abs(number);
-    let res = "";
-    do {
-        let digit = number % 2;
-        let sym = digit == 0 ? '.' : '-';
-        res = sym + res;
-        number = Math.trunc(number / 2);
+// function  fromMorseToNumber(morseCode) {
+//     let result = 0;
+//     for (let i = 0; i < morseCode.length; i++) {
+//         let code = morseCode[i] == '.' ? 0 : 1;
+//         result = result * 2 + code;
+//     }
+//     return result;
+// }
+// console.log(fromMorseToNumber('-.-...-.---.-.-..--'));
 
-    } while(number != 0);
-    return res;
-}
-console.log(fromNumberToMorse(333651));
+// function fromNumberToMorse(number) {
+//     number = Math.abs(number);
+//     let res = "";
+//     do {
+//         let digit = number % 2;
+//         let sym = digit == 0 ? '.' : '-';
+//         res = sym + res;
+//         number = Math.trunc(number / 2);
+
+//     } while(number != 0);
+//     return res;
+// }
+// console.log(fromNumberToMorse(333651));
 
 
 
@@ -109,4 +110,72 @@ console.log(fromNumberToMorse(333651));
 // console.log('abc'.charCodeAt(2))
 
 // //getting symbol from code
-// console.log(String.fromCharCode(126))
+// console.log(String.fromCharCode(900550))
+
+
+function fromNumberToString(value, code) {
+    var digit,
+        radix= code.length,
+        result = '';
+
+    do {
+        digit = value % radix;
+        result = code[digit] + result;
+        value = Math.floor(value / radix);
+    } while (value)
+
+    return result;
+}
+console.log(fromNumberToString(900550, '0123456789abcdefghijklmnopqrstuvwxyz')); //36
+console.log(fromNumberToString(46016237, '0123456789abcdefghijklmnopqrstuvwxyz')); //36
+console.log(fromNumberToString(46016237, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')); //36
+
+function fromStringToNumber(value, code) {
+    return [...value].reduce((r, a) => r * code.length + code.indexOf(a), 0);
+}
+console.log(fromStringToNumber('java', '0123456789abcdefghijklmnopqrstuvwxyz')); //36
+console.log(fromStringToNumber('react', '0123456789abcdefghijklmnopqrstuvwxyz')); //36
+// console.log(toString(11483, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')); 
+// console.log(parseInt('java', '0123456789abcdefghijklmnopqrstuvwxyz'));
+// console.log(parseInt('a', 'abcdefghijklmnopqrstuvwxyz0123456789+-'));
+// console.log(toString(142, 'abcdefghijklmnopqrstuvwxyz0123456789+-'));
+
+
+// function dec2bin(dec) {
+//     return (dec >>> 0).toString(2);
+//   }
+    // console.log(dec2bin(11483)); //2
+
+  function fromDecToBinary(v) {
+    // if (!Number.isSafeInteger(v) || v < 0) {
+    //   throw new Error('v must be a non-negative integer');
+    // }
+    if (v < 0) { 
+       return console.log("you entered negative integer"); 
+    }
+    if (v === 1) {
+      return '1';
+    }
+    if (v === 0) {
+      return '0';
+    }
+    return fromDecToBinary(Math.floor(v / 2)) + (v % 2);
+  }
+  console.log(fromDecToBinary(11483));
+
+  function changeBase(number, fromBase, toBase) {
+    if (fromBase == 10)
+        return (parseInt(number)).toString(toBase)
+    else if (toBase == 10)
+        return parseInt(number, fromBase);
+    else {
+        var numberInDecimal = parseInt(number, fromBase);
+        return parseInt(numberInDecimal).toString(toBase);
+    }
+}
+console.log(changeBase(10110011011011, 2, 10));
+
+console.log(changeBase(11483, 10, 2));
+  
+
+
