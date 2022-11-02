ce05e187e344060d240166f7ea0e7110295c4c97
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2022-11-02 21:01:06 +0300

    CW 13

--- app.js
+++ app.js
@@ -113,69 +113,106 @@ i = 100;
 // console.log(String.fromCharCode(900550))
 
 
-function fromNumberToString(value, code) {
-    var digit,
-        radix= code.length,
-        result = '';
+// function fromNumberToString(value, code) {
+//     var digit,
+//         radix= code.length,
+//         result = '';
 
+//     do {
+//         digit = value % radix;
+//         result = code[digit] + result;
+//         value = Math.floor(value / radix);
+//     } while (value)
+
+//     return result;
+// }
+// console.log(fromNumberToString(900550, '0123456789abcdefghijklmnopqrstuvwxyz')); //36
+// console.log(fromNumberToString(46016237, '0123456789abcdefghijklmnopqrstuvwxyz')); //36
+// console.log(fromNumberToString(46016237, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')); //36
+
+// function fromStringToNumber(value, code) {
+//     return [...value].reduce((r, a) => r * code.length + code.indexOf(a), 0);
+// }
+// console.log(fromStringToNumber('java', '0123456789abcdefghijklmnopqrstuvwxyz')); //36
+// console.log(fromStringToNumber('react', '0123456789abcdefghijklmnopqrstuvwxyz')); //36
+// // console.log(toString(11483, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')); 
+// // console.log(parseInt('java', '0123456789abcdefghijklmnopqrstuvwxyz'));
+// // console.log(parseInt('a', 'abcdefghijklmnopqrstuvwxyz0123456789+-'));
+// // console.log(toString(142, 'abcdefghijklmnopqrstuvwxyz0123456789+-'));
+
+
+// // function dec2bin(dec) {
+// //     return (dec >>> 0).toString(2);
+// //   }
+//     // console.log(dec2bin(11483)); //2
+
+//   function fromDecToBinary(v) {
+//     // if (!Number.isSafeInteger(v) || v < 0) {
+//     //   throw new Error('v must be a non-negative integer');
+//     // }
+//     if (v < 0) { 
+//        return console.log("you entered negative integer"); 
+//     }
+//     if (v === 1) {
+//       return '1';
+//     }
+//     if (v === 0) {
+//       return '0';
+//     }
+//     return fromDecToBinary(Math.floor(v / 2)) + (v % 2);
+//   }
+//   console.log(fromDecToBinary(11483));
+
+//   function changeBase(number, fromBase, toBase) {
+//     if (fromBase == 10)
+//         return (parseInt(number)).toString(toBase)
+//     else if (toBase == 10)
+//         return parseInt(number, fromBase);
+//     else {
+//         var numberInDecimal = parseInt(number, fromBase);
+//         return parseInt(numberInDecimal).toString(toBase);
+//     }
+// }
+// console.log(changeBase(10110011011011, 2, 10));
+
+// console.log(changeBase(11483, 10, 2));
+
+
+  
+function getSymbol(digit) {
+    let codeA = 'a'.charCodeAt();
+    return digit < 10 ? digit : String.fromCharCode(codeA + digit - 10)
+}
+function fromNumberToString(number, base) {
+    number = Math.abs(number);
+    let res = "";
     do {
-        digit = value % radix;
-        result = code[digit] + result;
-        value = Math.floor(value / radix);
-    } while (value)
+        let digit = number % base;
+        let sym = getSymbol(digit);
+        res = sym + res;
+        number = Math.trunc(number / base);
 
+    } while(number != 0);
+    return res;
+}
+console.log(fromNumberToString(900550,36));
+// console.log(900550 % 36);
+
+function getDigit(symbol) {
+    let codeA = 'a'.charCodeAt();
+    let res = symbol < '9' ? +symbol : symbol.charCodeAt() - codeA + 10
+    return res;
+}
+function  fromStringToNumber(string, base) {
+    string = string.toLowerCase();
+    let result = 0;
+    for (let i = 0; i <string.length; i++) {
+        let digit = getDigit(string[i]);
+        result = result * base + digit;
+    }
     return result;
 }
-console.log(fromNumberToString(900550, '0123456789abcdefghijklmnopqrstuvwxyz')); //36
-console.log(fromNumberToString(46016237, '0123456789abcdefghijklmnopqrstuvwxyz')); //36
-console.log(fromNumberToString(46016237, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')); //36
-
-function fromStringToNumber(value, code) {
-    return [...value].reduce((r, a) => r * code.length + code.indexOf(a), 0);
-}
-console.log(fromStringToNumber('java', '0123456789abcdefghijklmnopqrstuvwxyz')); //36
-console.log(fromStringToNumber('react', '0123456789abcdefghijklmnopqrstuvwxyz')); //36
-// console.log(toString(11483, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')); 
-// console.log(parseInt('java', '0123456789abcdefghijklmnopqrstuvwxyz'));
-// console.log(parseInt('a', 'abcdefghijklmnopqrstuvwxyz0123456789+-'));
-// console.log(toString(142, 'abcdefghijklmnopqrstuvwxyz0123456789+-'));
-
-
-// function dec2bin(dec) {
-//     return (dec >>> 0).toString(2);
-//   }
-    // console.log(dec2bin(11483)); //2
-
-  function fromDecToBinary(v) {
-    // if (!Number.isSafeInteger(v) || v < 0) {
-    //   throw new Error('v must be a non-negative integer');
-    // }
-    if (v < 0) { 
-       return console.log("you entered negative integer"); 
-    }
-    if (v === 1) {
-      return '1';
-    }
-    if (v === 0) {
-      return '0';
-    }
-    return fromDecToBinary(Math.floor(v / 2)) + (v % 2);
-  }
-  console.log(fromDecToBinary(11483));
-
-  function changeBase(number, fromBase, toBase) {
-    if (fromBase == 10)
-        return (parseInt(number)).toString(toBase)
-    else if (toBase == 10)
-        return parseInt(number, fromBase);
-    else {
-        var numberInDecimal = parseInt(number, fromBase);
-        return parseInt(numberInDecimal).toString(toBase);
-    }
-}
-console.log(changeBase(10110011011011, 2, 10));
-
-console.log(changeBase(11483, 10, 2));
-  
+console.log(fromStringToNumber("java", 36));
+console.log(fromStringToNumber("10", 16));
 
 
