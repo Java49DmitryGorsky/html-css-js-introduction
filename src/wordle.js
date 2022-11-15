dc79b757d40133e4c78f8544908fdfe7131d0e46
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2022-11-15 21:10:43 +0300

    HW #16

--- src/wordle.js
+++ src/wordle.js
@@ -16,7 +16,7 @@ function onChange(event) {
             let index = word.indexOf(l);
             let res = 'red';
             if (index > -1) {
-                res = index == i ? 'green' : 'yellow';
+                res = index == word.charAt(i) ? 'green' : 'yellow';
             }
             return res;
         })
