8db9526357cfdfa9bee810a39cb1f5b30ede7295
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2022-11-23 16:13:29 +0300

    HW #18 isAnagram

--- src/app.js
+++ src/app.js
@@ -1,19 +1,13 @@
-let button = document.getElementById('button-test');
-button.addEventListener('click', isAnagram);
-let resultDiv = document.getElementById('result');
-
-function isAnagram() {
-  let str1 = document.getElementById('string1').value;
-  let str2 = document.getElementById('string2').value;
+function isAnagram(str1, str2) {
   if (str1 !== null && str2 !== null) {
     if (str1.length !== str2.length) {
-      resultDiv.innerHTML = "Strings aren't anagrams!";
+      // resultDiv.innerHTML = "Strings aren't anagrams!";
       return false;
     }
     let hashTable = {};
     for (let i = 0; i < str1.length; i++) {
       if (hashTable.hasOwnProperty(str1[i])) {
-        hashTable[str1[i]] = hashTable[str1[i]] + 1;
+        hashTable[str1()[i]] = hashTable[str1[i]] + 1;
       } else {
         hashTable[str1[i]] = 1;
       }
@@ -23,18 +17,24 @@ function isAnagram() {
       if (hashTable.hasOwnProperty(str2[i])) {
         hashTable[str2[i]] = hashTable[str2[i]] - 1;
       } else {
-        resultDiv.innerHTML = "Strings aren't anagrams!";
+        // resultDiv.innerHTML = "Strings aren't anagrams!";
         return false;
       }
     }
 
     for (let i in hashTable) {
       if (hashTable[i] !== 0) {
-        resultDiv.innerHTML = "Strings aren't anagrams!";
+        // resultDiv.innerHTML = "Strings aren't anagrams!";
         return false;
       } else {
-        resultDiv.innerHTML = "True! Strings are anagrams!";
+        // resultDiv.innerHTML = "True! Strings are anagrams!";
+        return true;
       }
     }
   }
 }
+
+const str1 = 'CatAndDog';
+const str2 = 'DogAmdCat';
+
+console.log('Anagram',str2, 'for', str1, 'is', isAnagram(str1, str2))
