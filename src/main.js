1f09efb8c7c71e10de634f77f653839000d73c53
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2022-12-09 14:15:40 +0300

    HW #23 Library

---
+++ src/main.js
@@ -0,0 +1,98 @@
+import { Library } from "./data/library.js";
+const inputElements = document.querySelectorAll(".form-class [name]");
+const MIN_PAGES = 50;
+const MAX_PAGES = 2000;
+const TIME_OUT_ERROR_MESSAGE = 5000;
+const ERROR_CLASS = "error";
+
+const pagesErrorElement = document.getElementById("pages_error");
+const pagesFormErrorElement = document.getElementById("pages_form_error");
+const sectionsElement = document.querySelectorAll("section");
+
+const booksListElement = document.getElementById("books-all");
+const booksPagesListElement = document.getElementById("books-pages");
+
+const library = new Library();
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log("submitted");
+    const book = Array.from(inputElements).reduce((res, cur) => {
+        res[cur.name] = cur.value;
+        return res;
+    }, {}
+    )
+    console.log(book)
+    library.hireBook(book);
+
+}
+function onChange(event) {
+    if (event.target.name == "pages") {
+        validatePages(event.target)
+    }
+}
+function validatePages(element) {
+    const value = +element.value;
+    if (value < MIN_PAGES || value > MAX_PAGES) {
+        const message = value < MIN_PAGES ? `pages must be ${MIN_PAGES} or greater`
+            : `pages must be ${MAX_PAGES} or less`;
+        showErrorMessage(element, message, pagesErrorElement);
+    }
+}
+function showErrorMessage(element, message, errorElement) {
+    element.classList.add(ERROR_CLASS);
+    errorElement.innerHTML = message;
+    setTimeout(() => {
+        element.classList.remove(ERROR_CLASS);
+        element.value = '';
+        errorElement.innerHTML = '';
+    }, TIME_OUT_ERROR_MESSAGE);
+}
+let pagesFrom = 0;
+let pagesTo = 0;
+function onSubmitPages(event) {
+    event.preventDefault();
+    const books = library.getBooksByPages(pagesFrom, pagesTo);
+    booksPagesListElement.innerHTML = getBookItems(books);
+
+}
+function onChangePagesFrom(event) {
+    const value = +event.target.value;
+    if (pagesFrom && value >= pagesTo) {
+        showErrorMessage(event.target, "Pages 'from' must be less than Pages 'to'",
+            salaryFormErrorElement);
+    } else {
+        pagesFrom = value;
+    }
+}
+function onChangePagesTo(event) {
+    const value = +event.target.value;
+    if (pagesFrom && value < pagesFrom) {
+        showErrorMessage(event.target, "Pages 'To' must be greater than Pages 'From'",
+            pagesFormErrorElement);
+    }
+    pagesTo = value;
+}
+function show(index) {
+    sectionsElement.forEach(e => e.hidden = true)
+    sectionsElement[index].hidden = false;
+    if (index == 1) {
+        const books = library.getAllBooks();
+        booksListElement.innerHTML = getBookItems(books);
+    }
+}
+function getBookItems(books) {
+    return books.map(e =>
+        `<li class="books-item"><div class="books-item-container">
+                 <p class="books-item-paragraph">Author: ${e.author} </p>
+                 <p class="books-item-paragraph">Title: ${e.title} </p>
+                 <p class="books-item-paragraph">Genre: ${e.genre}</p>
+                 <p class="books-item-paragraph">Pages: ${e.pages}</p></li>`).join('');
+}
+
+window.onSubmit = onSubmit;
+window.onChange = onChange;
+window.show = show;
+window.onChangePagesTo = onChangePagesTo;
+window.onChangePagesFrom = onChangePagesFrom
+window.onSubmitPages = onSubmitPages
